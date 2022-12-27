ca1934be3baeb7d245ee77825a0a818d89607b4a
Nermeen-M <nermeen.mamdouh.nm@gmail.com>
2022-12-27 15:57:04 +0200

    About me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import About from "./components/About"
 
 import "./assets/styles/style.css"
 
@@ -8,7 +9,7 @@ export default function App() {
     <div className="App">
       <Header />
       <Hero />
-      <section id="about" style={{backgroundColor:"gray", height:"100vh"}}>About</section>
+      <About />
     </div>
   );
 }
